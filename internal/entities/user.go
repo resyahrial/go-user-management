0879f564be7543c2e47290f28d0425f59685f242
resyahrial <syahrialrangga11@gmail.com>
2022-11-12 18:19:21 +0700

    update usecase interface

--- internal/entities/user.go
+++ internal/entities/user.go
@@ -19,4 +19,5 @@ type UserUsecase interface {
 	Create(ctx context.Context, input *User) (user *User, err error)
 	Update(ctx context.Context, id string, input *User) (user *User, err error)
 	GetDetail(ctx context.Context, id string) (user *User, err error)
+	GetList(ctx context.Context, param *PaginatedQueryParams) (users []*User, count int64, err error)
 }
