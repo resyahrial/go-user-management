19dd16949e0281e59e808836f850e5e15adae777
resyahrial <syahrialrangga11@gmail.com>
2022-11-13 11:30:13 +0700

    create login usecase

--- internal/usecase/auth/init.go
+++ internal/usecase/auth/init.go
@@ -8,7 +8,7 @@ type AuthUsecaseImpl struct {
 	TokenHandler
 }
 
-func NewUserUsecase(
+func NewAuthUsecase(
 	userRepo UserRepo,
 	hash Hasher,
 	tokenHandler TokenHandler,
--- internal/usecase/auth/login.go
+++ internal/usecase/auth/login.go
@@ -4,8 +4,37 @@ import (
 	"context"
 
 	"github.com/resyahrial/go-user-management/internal/entities"
+	"github.com/resyahrial/go-user-management/pkg/exception"
+)
+
+var (
+	ErrInvalidLoginIput = exception.NewAuthenticationException().SetModule(entities.AuthModule).SetMessage("email / password is invalid")
 )
 
 func (u *AuthUsecaseImpl) Login(ctx context.Context, input *entities.Login) (token *entities.Token, err error) {
+	var (
+		user        *entities.User
+		accessToken string
+	)
+
+	if user, err = u.UserRepo.GetByEmail(ctx, input.Email); err != nil {
+		return
+	}
+
+	if ok := u.Hasher.CheckPasswordHash(input.Password, user.Password); !ok {
+		err = ErrInvalidLoginIput
+		return
+	}
+
+	if accessToken, err = u.TokenHandler.SignToken(map[string]interface{}{
+		"id": user.ID,
+	}); err != nil {
+		return
+	} else {
+		token = &entities.Token{
+			Access: accessToken,
+		}
+	}
+
 	return
 }
