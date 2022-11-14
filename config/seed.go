6248f438d81710be9bd81099d5738a112b6f4c2b
resyahrial <syahrialrangga11@gmail.com>
2022-11-14 18:16:22 +0700

    create seed admin

--- config/seed.go
+++ config/seed.go
@@ -1,7 +1,7 @@
 package config
 
 type Seed struct {
-	FirstAdmin struct {
+	Admin struct {
 		Email    string `yaml:"email" env:"SEED_ADMIN_EMAIL"`
 		Password string `yaml:"password" env:"SEED_ADMIN_PASSWORD"`
 	} `yaml:"admin"`
